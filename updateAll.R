ff8df27c2c89adebd410a85ec882b7f92db6dc81
Ben Anderson <dataknut@icloud.com>
2023-01-25 14:23:14 +0000

    switched to the single integrated dsfReport.qmd

--- updateAll.R
+++ updateAll.R
@@ -1,16 +1,22 @@
 # updates all qmd
 library(quarto)
 
-quarto::quarto_render(input = "savingSessionsTweets.qmd",
+quarto::quarto_render(input = "dfsReport.qmd",
               output_format = c("html")
 )
 
-quarto::quarto_render(input = "demandFlexibilityServiceTweets.qmd",
-                      output_format = c("html")
-)
+# we no longer update these as the code is combined in dfsReport.qmd
 
-quarto::quarto_render(input = "savingSessionsEnergy.qmd",
-                      output_format = c("html")
-)
+# quarto::quarto_render(input = "demandFlexibilityServiceTweets.qmd",
+#                       output_format = c("html")
+# )
+# 
+# quarto::quarto_render(input = "savingSessionsTweets.qmd",
+#                       output_format = c("html")
+# )
+# 
+# quarto::quarto_render(input = "savingSessionsEnergy.qmd",
+#                       output_format = c("html")
+# )
 
 # auto git commit & push?
